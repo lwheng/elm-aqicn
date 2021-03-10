6afef757973e79c58ff31d695b96a9890de2201b
Low-Wee Heng <alpha116@gmail.com>
2021-03-10 22:11:21 +0800

    Switch to Browser.application

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,18 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Navigation as BN
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Url exposing (Url)
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.application { init = init, view = view, update = update, subscriptions = subscriptions, onUrlRequest = onUrlRequest, onUrlChange = onUrlChange }
 
 
 type alias Model =
     Int
-
-
-init : Model
-init =
-    0
 
 
 type Msg
@@ -23,20 +20,44 @@ type Msg
     | Decrement
 
 
-update : Msg -> Model -> Model
+init : Int -> Url -> BN.Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( 0, Cmd.none )
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Hello World"
+    , body =
+        [ div []
+            [ button [ onClick Decrement ] [ text "-" ]
+            , div [] [ text (String.fromInt model) ]
+            , button [ onClick Increment ] [ text "+" ]
+            ]
+        ]
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            ( model + 1, Cmd.none )
 
         Decrement ->
-            model - 1
+            ( model - 1, Cmd.none )
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest _ =
+    Increment
+
+
+onUrlChange : Url -> Msg
+onUrlChange _ =
+    Increment
